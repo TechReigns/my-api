5455cf1a4729a333081dc7c89e0cdca5e20f5832
Selena Munoz-Reign <techreigns0515@gmail.com>
2021-04-15 14:17:58 -0500

    setup api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+// The REST API endpoint
+const API_URL = 'api/v1/plants?token=qrbYXSsfNwVJLOQI24d-itX0n3dPO3GJ5veUkHz1J7o';
+
+const App = () => {
+  // At the beginning, posts is an empty array
+  const [posts, setPosts] = useState([]);
+
+  // Define the function that fetches the data from API
+  const fetchData = async () => {
+    const { data } = await axios.get(API_URL);
+    setPosts(data);
+  };
+
+  // Trigger the fetchData after the initial render by using the useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {posts.length > 0 ? (
+        <div className="content">
+          {posts.map((post) => (
+            <div className="post">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="loading">Loading... </p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
